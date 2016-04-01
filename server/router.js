a3e0c7196f46eb9bc2428c283c6abf8195c75920
Sterv <stephensulliv@gmail.com>
2016-04-01 10:52:53 -0700

    Adding coordinates request handler to routes.js

--- server/router.js
+++ server/router.js
@@ -1,28 +1,15 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var coordinates = require('coordinates');
 
 app.use(bodyParser.json());
 
-
-app.route('/location')
-  .get(function(req, res) {
-    res.send('look ma! A mutation!');
-  })
-  .post(function(req, res) {
+app.post('/', function(req, res) {
+    coordinates.requestHandler(req,res);
     res.send('but we has no data!!!');
   });
-app.route('/duration')
-  .get(function(req, res) {
-    res.send('CHYEAH');
-  })
-  .post(function(req, res) {
-    res.send('but we has no data');
-  });
-app.route('/category')
-  .get(function(req, res) {
-    res.send('ANGULAR!!');
-  })
-  .post(function(req, res) {
-    res.send('but we has no data');
-  })
+
+
+
+
