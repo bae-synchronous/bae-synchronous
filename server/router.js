5b00e25c3af86e01f65162c550fd50945704200e
Sterv <stephensulliv@gmail.com>
2016-04-01 11:05:24 -0700

    Fixed require statement for coordinates.js

--- server/router.js
+++ server/router.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-var coordinates = require('coordinates');
+var coordinates = require('./coordinates.js');
 
 app.use(bodyParser.json());
 
