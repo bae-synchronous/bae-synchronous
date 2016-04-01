ab64b91c8ca82812baa74d6b338e9d5c861e596a
Loren Barrick <lorenbarrick@gmail.com>
2016-03-31 19:53:38 -0700

    added routes

---
+++ server/router.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.json());
+
+
+app.route('/location')
+  .get(function(req, res) {
+    res.send('look ma! A mutation!');
+  })
+  .post(function(req, res) {
+    res.send('but we has no data!!!');
+  });
+app.route('/duration')
+  .get(function(req, res) {
+    res.send('CHYEAH');
+  })
+  .post(function(req, res) {
+    res.send('but we has no data');
+  });
+app.route('/category')
+  .get(function(req, res) {
+    res.send('ANGULAR!!');
+  })
+  .post(function(req, res) {
+    res.send('but we has no data');
+  })
