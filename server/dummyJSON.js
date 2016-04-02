fa4cadca1baaa28fc34f6253fcd836df8aa7a3dc
Sterv <stephensulliv@gmail.com>
2016-04-01 17:24:13 -0700

    Undergoing testing: chaining each API call

---
+++ server/dummyJSON.js
@@ -0,0 +1,37 @@
+var dummyDataObject = {
+    address1: {
+        address: '1600+Amphitheatre+Parkway,+Mountain+View,+CA',
+        coordinates: {
+            lat: 12312312,
+            lng: 12341324
+        }
+    },
+    address2: {
+        address: 'Mountain+View+High+School,+Truman Avenue,+Mountain+View,+CA,+United States',
+        coordinates: {
+            lat: 12312312,
+            lng: 12341324
+        }
+    },
+    thirdPoint: {
+        address: 'Mountain+View+High+School,+Truman Avenue,+Mountain+View,+CA,+United States',
+        coordinates: {
+            lat: 12312312,
+            lng: 12341324
+        }
+    },
+    category: 'gym',
+    maxTime: 30,
+    categoryListing: [
+        {
+            address: 'Mountain+View+High+School,+Truman Avenue,+Mountain+View,+CA,+United States',
+            coordinates: {
+                lat: 12312312,
+                lng: 12341324
+            },
+            places_id: 1242141,
+            timeFromAddress1: 12,
+            timeFromAddress2: 42
+        }
+    ]
+};
