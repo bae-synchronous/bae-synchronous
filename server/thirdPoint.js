cc004a19300bcbe4c59a10babf94fff0b650e135
Sterv <stephensulliv@gmail.com>
2016-04-03 11:18:48 -0700

    Moved third point function to seperate module

---
+++ server/thirdPoint.js
@@ -0,0 +1,16 @@
+// currently implementing the midpoing formula to calculate a third point
+function getThirdPoint (coordinates1, coordinates2) {
+    return findMidpoint(coordinates1, coordinates2);
+}
+
+function findMidpoint(coordinates1,coordinates2){
+    var thirdPoint = {};  
+    thirdPoint.lat = ((coordinates1.lat + coordinates2.lat)/2);
+    thirdPoint.lng = ((coordinates1.lng + coordinates2.lng)/2);
+    return thirdPoint;
+}
+
+module.exports = {
+    getThirdPoint: getThirdPoint,
+    findMidpoint: findMidpoint
+}
