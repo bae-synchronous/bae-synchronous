fcb7855c51bdc3c22117c7294036708235b10d3c
Sterv <stephensulliv@gmail.com>
2016-04-03 12:08:57 -0700

    Updated router.js to call request handler from different file and export request handler function from request_handler.js

--- server/coordinates.js
+++ server/coordinates.js
@@ -3,10 +3,10 @@ var dummy = require('./dummyData');
 var getThirdPoint = require('./thirdPoint').getThirdPoint;
 var helper = require('./helper');
 
-var API_KEY = 'AIzaSyAvXHQtnUPWtvPzT2M3u2VD1Pxqi7ihyfQ';
+var API_KEY ='AIzaSyAvXHQtnUPWtvPzT2M3u2VD1Pxqi7ihyfQ';
 
 // retreives geographic coordinates for an address
-function getCoordinates(address) {
+function getCoordinates(address){
   address = helper.replaceEmptySpaces(address);
 
   return axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
@@ -21,18 +21,18 @@ function getCoordinates(address) {
       return coordinatesObj;
     })
     .catch(function (response) {
-      console.log('error', response);
+      console.log('error',response);
       return 'enter a valid address';
     });
 }
 
 // requests a list of "places" based on coordinates (in this case, the "third point")
 // and a type (e.g. "gym") within a radius (measured in meters)
-function getPlaces(coordinates, radius, type, name) {
+function getPlaces(coordinates,radius,type,name){
 
-  coordinates = (typeof coordinates !== 'string') ? helper.stringifyCoordinates(coordinates) : coordinates;
+    coordinates = (typeof coordinates !== 'string')? helper.stringifyCoordinates(coordinates): coordinates;
 
-  axios.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json', {
+    axios.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json', {
       params: {
         location: coordinates,
         radius: radius,
@@ -48,26 +48,26 @@ function getPlaces(coordinates, radius, type, name) {
       return places;
     })
     .catch(function (response) {
-      console.log('error catch, places', response);
+      console.log('error catch, places',response);
     });
 }
 
 // this chains all of the functions below
-var getCoordinatesForEachAddress = function (address1, address2) {
+var getCoordinatesForEachAddress = function (address1,address2){
 
-  var promises = [getCoordinates(address1), getCoordinates(address2)];
+    var promises = [getCoordinates(address1), getCoordinates(address2)];
 
-  axios.all(promises)
-    .then(axios.spread(function (coordinatesObj1, coordinatesObj2) {
+    axios.all(promises)
+      .then(axios.spread(function (coordinatesObj1, coordinatesObj2) {
 
-      var thirdPoint = getThirdPoint(coordinatesObj1, coordinatesObj2);
-      thirdPoint = stringifyCoordinates(thirdPoint);
+        var thirdPoint = getThirdPoint(coordinatesObj1,coordinatesObj2);
+        thirdPoint = stringifyCoordinates(thirdPoint);
 
-      return getPlaces(thirdPoint);
+        return getPlaces(thirdPoint);
 
-    })).then(function (places) {
-      console.log('This will return an array of ', places);
-    });
+      })).then(function(places){
+        console.log('This will return an array of ',places);
+      });
 };
 
 // forTesting
