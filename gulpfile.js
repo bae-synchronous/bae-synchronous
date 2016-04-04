1e6df1095c1f82ddee43c1fcbb4f396011343cd7
Sheryl Hohman <SherylHohman@users.noreply.github.com>
2016-04-04 12:44:02 -0700

    Revert "Sheryl2: NOTE this is "sheryl2" branch not Sheryl - (had issues with Sheryl branch)"

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,7 @@ var browserSync = require('browser-sync').create();
 var watch = require('gulp-watch');
 
 gulp.task('lint', function() {
-  return gulp.src([
-    './*.js',
-    './public/**/.js',
-    './public/*.js',
-    './server/*.js',
-    './test/*.js'
-  ])
+  return gulp.src('index.js')
     .pipe(jshint())
     .pipe(jshint.reporter('default'))
     .pipe(jshint.reporter('fail'));
@@ -22,21 +16,36 @@ gulp.task('develop', function () {
   nodemon({
     script: 'index.js',
     ext: 'html js',
-    env: { 'NODE_ENV': 'development' },
-    tasks: ['lint', 'test']
+    env: { 'NODE_ENV': 'development' }
   })
-  .on('restart', function () {
-    console.log('restarted!');
-  });
+    .on('restart', function () {
+      console.log('restarted!')
+    })
 });
 
 gulp.task('test', function () {
-  gulp.src('test/**.js')
-    .pipe(mocha({
-      clearRequireCache: true,
-      ignoreLeaks: true
-  }));
+    gulp.src('test/**.js')
+        .pipe(mocha({
+            reporter: 'nyan',
+            clearRequireCache: true,
+            ignoreLeaks: true
+        }));
 });
+
+gulp.task('serve', function () {
+
+    // Serve files from the root of this project
+    browserSync.init({
+        server: {
+            baseDir: "./public"
+        }
+    });
+});
+
+gulp.task('watch', ['serve'], function(){
+    gulp.watch("*.**").on("change", browserSync.reload);
+})
+
 
 // Default Task
 gulp.task('default', ['lint', 'test', 'develop']);
--- index.js
+++ index.js
@@ -5,14 +5,6 @@ var bodyParser = require('body-parser');
 app.set('port', (process.env.PORT || 8000));
 
 app.use(express.static('public'));
-app.use(bodyParser.json());
-
-app.post('/', function(req, res) {
-  console.log(req.body);
-    // coordinates.requestHandler(req,res);
-    // sheryl.requestHandler(req,res);
-    // res.send('but we has no data!!!');
-});
 
 app.listen(app.get('port'), function () {
     console.log('bae-synchronous is running on port ', app.get('port'));
