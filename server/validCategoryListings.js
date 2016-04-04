eae54992a7dfc19d72d330ea5cccec4b3ab669f8
Sheryl Hohman <sherylhohman@yahoo.com>
2016-04-03 20:08:58 -0700

    WIP form query string to get category distances, and clean up file

--- server/validCategoryListings.js
+++ server/validCategoryListings.js
@@ -1,6 +1,11 @@
 // will probably rename this to validCategoryList.js
 var axios = require('axios');
-var dummyJSON = require('dummyJSON');
+var config = require('config');
+//var dummyJSON = require('dummyJSON');
+var format_of_returned_data = require('format_of_returned_data');
+var dummyData_in_our_format = require('dummyData_in_our_format');
+var dummyData_list_of_categories = require('dummyData_list_of_categories');
+// var dummyData = require('dummyData');
 
 function requestHandler(req,res){
   module.exports = {
@@ -9,82 +14,63 @@ function requestHandler(req,res){
 }
 
 
-// want these to be Global, no Window on server, dunno how to explicitly call them out
+// want these to be Global, no Window on server, dunno how to
+//explicitly call them out as global (to this file) vars
 
 // GoogleMapsDistanceMatrixAPI_base uri
-var base_url = "https://maps.googleapis.com/maps/api/distancematrix/";
-// shAPIkey_googleDistanceMatrixAPI
-var key = "AIzaSyAY1OTlXsTvjom2o_CQUDH1BLo8JVolp84";
+var base_url = "https://maps.googleapis.com/maps/api/distancematrix/json";
+var API_KEY  = config.shAPIkey_googleDistanceMatrixAPI;
 
-/// AXIOS EXAMPLE for building query
-function getPlaces(coordinates,radius,type,name){
-  coordinates = coordinates || dummyCoordinates;
-  radius = radius || dummyRadius;
-  type = type || dummyType;
-  name = name || dummyName;
-  axios.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json', {
-      params: {
-        location: coordinates,
-        radius: radius,
-        type: type,
-        // name: name,
-        key: API_KEY
-      }
-    })
-    .then(function (response) {
-      var places = response.data.results;
-      console.log('places',places);
-      return places;
-    })
-    .catch(function (response) {
-      console.log(response);
-    });
-}
+function getDurations(originAddress, originNumber, categoryObjects){
 
+  // build destination section of query parameter
+  var destinations = '';
+  var space = '%2C';
+  var pipe  = '%7C';
 
+  _.each(dummyData_list_of_categories, function(categoryListing){
+    destination += coordinates.lat + space + coordinates.lng + pipe;
+  })
 
-// temp data, and format of contract
-var dummyObject = {
-    address1: {
-        address: '1600+Amphitheatre+Parkway,+Mountain+View,+CA'
-        coordinates: {
-            lat: 12312312,
-            lng: 12341324
-        }
-    },
-    address2: {
-        address: 'Mountain+View+High+School,+Truman Avenue,+Mountain+View,+CA,+United States'
-        coordinates: {
-            lat: 12312312,
-            lng: 12341324
-        }
-    },
-    thirdPoint: {
-        address: 'Mountain+View+High+School,+Truman Avenue,+Mountain+View,+CA,+United States'
-        coordinates: {
-            lat: 12312312,
-            lng: 12341324
-        }
-    },
-    category: 'gym',
-    maxTime: 30,
-    categoryListing: [
-        {
-            address: 'Mountain+View+High+School,+Truman Avenue,+Mountain+View,+CA,+United States'
-            coordinates: {
-                lat: 12312312,
-                lng: 12341324
-            },
-            places_id: 1242141,
-            timeFromAddress1: 12,
-            timeFromAddress2: 42
-        }
-    ];
+    // AXIOS EXAMPLE for building query
+      // axios.get(base_url,
+      //           params: {
+      //             key = API_KEY,
+      //             units = imperial
+      //             origins = { address1.coordinates.lat,
+      //                         address1.coordinates.lng
+      //                       }
+      //             destinations = destinations;
+      //           })
+      //           .then(function (response) {
+      //             var places = response.data.results;
+      //             console.log('places',places);
+      //             return places;
+      //           })
+      //           .catch(function (response) {
+      //             console.log(response);
+      //           });
+
+  var queryString = '' + '?' + 'units=imperial' +
+                         '&' + 'origins=' + address1.coordinates.lat + ',' + address1.coordinates.lng +
+                         '&' + 'destinations=' + destinations +
+                         '&' + 'key=' + API_KEY;
+
+  var google_req = base_url + queryString;
+
+  // make our query
+  // say our data is returned in a 'data' object
+
+  // populate our data with the commute time we received back from this query
+  // dummyData_list_of_categories
+
 }
 
 
 /*
-Function: takes in (array of categoryLocations, address). Returns (array of {address, categoryAddress, duration})  where travelTime represents the time from address to that categoryAddress.
+  Function: takes in (array of categoryLocations, address).
+  Returns (array of {address, categoryAddress, duration})
+  where travelTime represents the time from address to that categoryAddress.
 */
 
 // first add fields to categoryObjects:
@@ -92,14 +78,14 @@ Function: takes in (array of categoryLocations, address). Returns (array of {add
 
 
 
-function
-// now add addressField to arrayOfCategories
-  // and populate it
-// maybe: add property fields, initialized to null
-  // address1
-  // address2
-  // durationAddress1
-  // durationAddress2
+// function:
+  // now add addressField to arrayOfCategories
+    // and populate it
+  // maybe: add property fields, initialized to null
+    // address1
+    // address2
+    // durationAddress1
+    // durationAddress2
 
 
 function getDurations(originAddress, originNumber, categoryObjects){
@@ -127,35 +113,6 @@ function getDurations(originAddress, originNumber, categoryObjects){
         // add originAddress to this Object as well
         // (we may want to display )
 
-  var 
-
-  // GoogleDistanceMatrixAPIrequestString
-  var google_req = base_url + "json?" + "units=imperial";
-  google_req += "origins="
-
-  var durToAddr = "durationToAddress" + originNumber;
-  var originLatLong = addressToLatLong(originAddress);
-  _.each(categoryObjects, function(categoryObject){
-
-  }){};
-https://maps.googleapis.com/maps/api/distancematrix/json?
-units=imperial
-&origins=40.6655101,-73.89188969999998
-&destinations=40.6905615%2C-73.9976592%7C
-              40.6905615%2C-73.9976592%7C
-              40.6905615%2C-73.9976592%7C
-              40.6905615%2C-73.9976592%7C
-              40.6905615%2C-73.9976592%7C
-              40.6905615%2C-73.9976592%7C
-              40.659569%2C-73.933783%7C
-              40.729029%2C-73.851524%7C
-              40.6860072%2C-73.6334271%7C
-              40.598566%2C-73.7527626%7C4
-              0.659569%2C-73.933783%7C
-              40.729029%2C-73.851524%7C
-              40.6860072%2C-73.6334271%7C
-              40.598566%2C-73.7527626
-&key=YOUR_API_KEY
 
 
 /* Object returned to Client will ultimately look KINDA like:
