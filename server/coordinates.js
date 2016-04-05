5590a26ecb4594e6001b72c5e60d51e036b6f37c
Loren Barrick <lorenbarrick@gmail.com>
2016-04-05 14:35:52 -0700

    this thing

--- server/coordinates.js
+++ server/coordinates.js
@@ -82,7 +82,6 @@ function getPlacesForAddresses(address1, address2, radius, type) {
 // forTesting
 // getPlaces({ lat: -33.868981950000006, lng: 151.1958316 },dummyRadius,dummyType,dummyName)
 
-
 module.exports = {
   getCoordinates: getCoordinates,
   getCoordinatesForEachAddress: getCoordinatesForEachAddress,
