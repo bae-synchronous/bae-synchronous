cb286a72133a050d05a7432c67627b5e7c84fb47
Dylan Hughes <dylanbhughes@gmail.com>
2016-04-06 14:18:51 -0700

    displays category listings.

--- index.js
+++ index.js
@@ -10,20 +10,20 @@ app.set('port', (process.env.PORT || 8000));
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
+console.log(dummyData);
+
 app.post('/', function(req, res) {
 
 });
 
 app.post('/places', function(req, res) {
   // requestHandler(req, res);
+  // res.writeHead(200, {'Content-Type': 'application/JSON'});
   dummyData = JSON.stringify(dummyData);
+  // console.log(dummyData);
   res.send(dummyData);
 });
 
 app.listen(app.get('port'), function () {
     console.log('bae-synchronous is running on port ', app.get('port'));
 });
-
-
-// var test = require('./server/validCategoryListings');
-// test.getValidCategoryListings();
