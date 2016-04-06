1407142d5eb95eb6dd4500787c15f911a34db972
Sterv <stephensulliv@gmail.com>
2016-04-06 11:23:18 -0700

    Public/app.js bug fix on GET request callback

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var requestHandler = require('./server/request_handler.js');
-var dummyData = require('./server/dummyData/categoryListings_from_Steve')
+var dummyData = require('./server/dummyData/categoryListings_from_Steve');
 
 app.set('port', (process.env.PORT || 8000));
 
@@ -10,20 +10,13 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 
 app.post('/', function(req, res) {
-  console.log(req.body);
-    // coordinates.requestHandler(req,res);
-    // sheryl.requestHandler(req,res);
-    // res.send('but we has no data!!!');
+    
 });
-
 
 app.post('/places', function(req, res) {
   // requestHandler(req, res);
-  console.log('dummyData',dummyData);
-  dummyData = JSON.stringify(dummyData)
+  dummyData = JSON.stringify(dummyData)  
   res.send(dummyData);
-
-
 });
 
 app.listen(app.get('port'), function () {
