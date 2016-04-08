aff78da060a9f5a6e560834cfcb5a6e6a5d77df9
Sterv <stephensulliv@gmail.com>
2016-04-08 13:55:33 -0700

    Refactored API calls, reformated response object to reflect API contract

--- server/request_handler.js
+++ server/request_handler.js
@@ -2,14 +2,10 @@
 var coordinates = require('./coordinates');
 
 function requestHandler(req,res){
-  var address1 = req.body.address1;
-  var address2 = req.body.address2;
-  var category = req.body.category;
-  var duration = req.body.duration;
 
-  coordinates.getPlacesFromThirdPoint(address1, address2, category,duration)
+  coordinates.makeAPICalls(req)
     .then(function(response) {
-        console.log('final response');
+        console.log('API calls went through!');
         res.send(response);
     });
 }
