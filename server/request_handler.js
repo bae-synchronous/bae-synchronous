8c282c6b2ecb4fa7dc3fcc227624ccbdc24de618
Sheryl Hohman <sherylhohman@yahoo.com>
2016-04-09 13:34:34 -0700

    return dummy data from request handler

--- server/request_handler.js
+++ server/request_handler.js
@@ -5,13 +5,17 @@ function requestHandler(req,res){
   var address1 = req.body.address1;
   var address2 = req.body.address2;
   var category = req.body.category;
-  var duration = req.body.duration;
+  var duration = req.body.maxTime;
 
-  coordinates.getPlacesFromThirdPoint(address1, address2, category,duration)
-    .then(function(response) {
-        console.log('final response');
-        res.send(response);
-    });
+  // coordinates.getPlacesFromThirdPoint(address1, address2, category,duration)
+  //   .then(function(response) {
+  //       console.log('final response');
+  //       res.send(response);
+  //   });
+
+  var data = require(./dummyData/data_we_return_to_client.js’);
+  response = data;
+  res.send(response);
 }
 
 module.exports = requestHandler;
