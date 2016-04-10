85b10f8fd3fbf07efe037051d8dc1c1d6b2ca28f
Sheryl Hohman <sherylhohman@yahoo.com>
2016-04-10 10:26:39 -0700

    fix bug, comment out injected  SH code from coordinates

--- server/coordinates.js
+++ server/coordinates.js
@@ -1,10 +1,18 @@
 var axios = require('axios');
-var dummy = require('./dummyData/data_we_return_to_client');
+// var dummy = require('./dummyData/data_we_return_to_client');
 var getThirdPoint = require('./thirdPoint').getThirdPoint;
 var helper = require('./helper');
-var _ = require('underscore');
+// var _ = require('underscore');
 
 var API_KEY ='AIzaSyAvXHQtnUPWtvPzT2M3u2VD1Pxqi7ihyfQ';
+
+//testing
+var fromSteve = getPlacesFromThirdPoint('50 Murray Street, Pyrmont',
+                        '37 Pyrmont Street, Pyrmont',
+                        'gym',
+                        30);
+console.log('results from Steve:\n\n', fromSteve);
+/////
 
 // retreives geographic coordinates for an address
 function getCoordinates(address){
@@ -55,7 +63,7 @@ function getPlaces(coordinates,radius,type,name){
 }
 
 // this chains all of the functions below
-var getCoordinatesForEachAddress = function (address1,address2){
+function getCoordinatesForEachAddress(address1,address2){
 
     var promises = [getCoordinates(address1), getCoordinates(address2)];
 
@@ -90,7 +98,8 @@ function getPlacesFromThirdPoint(address1, address2, category,duration) {
   };
 
   return new Promise(function(resolve, reject) {
-    getCoordinatesForEachAddress(address1, address2).then(function(resp) {
+    getCoordinatesForEachAddress(address1, address2)
+    .then(function(resp) {
 
       response.address1.coordinates = resp.coordinatesObj1;
       response.address2.coordinates = resp.coordinatesObj2;
@@ -98,83 +107,85 @@ function getPlacesFromThirdPoint(address1, address2, category,duration) {
 
       // console.log('thirdPoint:',thirdPoint);
 
-      getPlaces(thirdPoint, radius, category).then(function(places) {
+      getPlaces(thirdPoint, radius, category)
+    .then(function(places) {
         console.log('1');
-          addCommuteTimes(places).then(function(places){
+          // addCommuteTimes(places).then(function(places){
             console.log('2');
             response.categoryListings = places;
+            console.log(response.categoryListings);
             resolve(response);
           });
       });
     });
-  });
+  // });
 }
 
-function getCommuteTime(origins,destinations){
-    // origins will be address1 & address2
-    // destinations will be each "place"
-    origins = origins || "-33.870353,151.197892|-33.8676109,151.1937712"; // this is address1
-    destinations = destinations ||"-33.8676109,151.1937712|-33.87229689999999,151.1979047"; // this is address2
+// function getCommuteTime(origins,destinations){
+//     // origins will be address1 & address2
+//     // destinations will be each "place"
+//     origins = origins || "-33.870353,151.197892|-33.8676109,151.1937712"; // this is address1
+//     destinations = destinations ||"-33.8676109,151.1937712|-33.87229689999999,151.1979047"; // this is address2
 
-    return axios.get('https://maps.googleapis.com/maps/api/distancematrix/json', {
-      params: {
-        units: 'imperial',
-        origins: origins,
-        destinations: destinations,
-        key: 'AIzaSyD-vG8sAwH0fpsObvWxWoSou2LJe-ZbV1s'
-      }
-    })
-    .then(function (response) {
-      var commutes = response.data;
-      return commutes;
-    })
-    .catch(function (response) {
-      console.log('error',response);
-    });
-}
+//     return axios.get('https://maps.googleapis.com/maps/api/distancematrix/json', {
+//       params: {
+//         units: 'imperial',
+//         origins: origins,
+//         destinations: destinations,
+//         key: 'AIzaSyD-vG8sAwH0fpsObvWxWoSou2LJe-ZbV1s'
+//       }
+//     })
+//     .then(function (response) {
+//       var commutes = response.data;
+//       return commutes;
+//     })
+//     .catch(function (response) {
+//       console.log('error',response);
+//     });
+// }
 
-function createDestinationsString(places){
-  return places.map(function(place){
-    return helper.stringifyCoordinates(place.geometry.location);
-  }).join('|');
-}
+// function createDestinationsString(places){
+//   return places.map(function(place){
+//     return helper.stringifyCoordinates(place.geometry.location);
+//   }).join('|');
+// }
 
-function createOriginsString(address1,address2){
-  return helper.stringifyCoordinates(address1) + '|' + helper.stringifyCoordinates(address2);
-}
+// function createOriginsString(address1,address2){
+//   return helper.stringifyCoordinates(address1) + '|' + helper.stringifyCoordinates(address2);
+// }
 
-function extendPlacesObject(places){
-  var origins = createOriginsString(dummy.address1.coordinates, dummy.address2.coordinates);
-  var destinations = createDestinationsString(places);
+// function extendPlacesObject(places){
+//   var origins = createOriginsString(dummy.address1.coordinates, dummy.address2.coordinates);
+//   var destinations = createDestinationsString(places);
 
-  return getCommuteTime(origins,destinations).then(function(commutes){
-    extendPlaceObject(places,commutes);
-    return places;
-  });
-}
+//   return getCommuteTime(origins,destinations).then(function(commutes){
+//     extendPlaceObject(places,commutes);
+//     return places;
+//   });
+// }
 
-function extendPlaceObject(places,commutes){
+// function extendPlaceObject(places,commutes){
 
-  _.each(places,function(place,idx){
-    var commuteProps = {
-      fromAddress1: commutes.rows[0].elements[idx].duration.text,
-      fromAddress2: commutes.rows[1].elements[idx].duration.text
-    };
-    _.extend(place,commuteProps);
-  });
+//   _.each(places,function(place,idx){
+//     var commuteProps = {
+//       fromAddress1: commutes.rows[0].elements[idx].duration.text,
+//       fromAddress2: commutes.rows[1].elements[idx].duration.text
+//     };
+//     _.extend(place,commuteProps);
+//   });
 
-  return places;
-}
+//   return places;
+// }
 
-function addCommuteTimes(places){
-  var destinations = createDestinationsString(places);
-  var origins = createOriginsString(dummy.address1.coordinates, dummy.address2.coordinates);
+// function addCommuteTimes(places){
+//   var destinations = createDestinationsString(places);
+//   var origins = createOriginsString(dummy.address1.coordinates, dummy.address2.coordinates);
 
-  return getCommuteTime(origins,destinations).then(function(commutes){
-    extendPlaceObject(places,commutes);
-    return places;
-  });
-}
+//   return getCommuteTime(origins,destinations).then(function(commutes){
+//     extendPlaceObject(places,commutes);
+//     return places;
+//   });
+// }
 
 module.exports = {
   getCoordinates: getCoordinates,
